21b682ef05bea31b9d3da38cd0a1d806aaddec9e
David Moreno García <david.mogar@gmail.com>
2017-06-18 23:51:32 +0200

    Update site with new pages and styles

--- js/cucco.js
+++ js/cucco.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
   document.getElementById('hamburger').addEventListener('click', function(event) {
     document.getElementsByTagName('nav')[0].classList.toggle('expanded');
   }, false);
+
+  window.addEventListener('click', function(e){
+    if (!document.getElementsByTagName('header')[0].contains(e.target)) {
+      document.getElementsByTagName('nav')[0].classList.remove('expanded');
+    }
+  });
 });
