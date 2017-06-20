53d62327aeeaa84bfedac74966dc00e8dbd0aa87
David Moreno García <david.mogar@gmail.com>
2017-06-20 11:50:42 +0200

    Update site with new pages and styles

--- js/cucco.js
+++ js/cucco.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-
   document.getElementById('hamburger').addEventListener('click', function(event) {
     document.getElementsByTagName('nav')[0].classList.toggle('expanded');
   }, false);
@@ -7,6 +6,23 @@ document.addEventListener('DOMContentLoaded', function(event) {
   window.addEventListener('click', function(e){
     if (!document.getElementsByTagName('header')[0].contains(e.target)) {
       document.getElementsByTagName('nav')[0].classList.remove('expanded');
+    }
+  });
+
+  var goUpButton = document.getElementById('go-up');
+  var goUpButtonVisible = false;
+
+  goUpButton.addEventListener('click', function(event) {
+    window.scrollTo(0, 0);
+  }, false);
+
+  window.addEventListener('scroll', function(e) {
+    if (!goUpButtonVisible && window.scrollY > 1000) {
+      goUpButtonVisible = true;
+      goUpButton.classList.add('visible');
+    } else if (goUpButtonVisible && window.scrollY < 800) {
+      goUpButtonVisible = false;
+      goUpButton.classList.remove('visible');
     }
   });
 });
